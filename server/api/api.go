9a3bda2095c55a5d400ad2d7045f863e26158134
Liv Ghit <livghit@Livs-MacBook-Pro.local>
2023-07-11 08:06:13 +0200

    Updated readme

--- server/api/api.go
+++ server/api/api.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 func HandleSearch(c echo.Context) error {
+	time.Sleep(time.Second * 5)
 	name := c.Param("name")
 	return c.JSON(http.StatusOK, name)
 }
