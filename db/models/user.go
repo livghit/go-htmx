4e6f7e9c88f161759c54cded7862c0e03c03d0e8
livghit <liviu.g01@gmail.com>
2024-07-15 12:47:57 +0200

    wip

--- db/models/user.go
+++ db/models/user.go
@@ -1,4 +1,22 @@
 package models
 
 // defining the models
-type User struct{}
+type User struct {
+	Id       int
+	Username string
+	Password string
+	Email    string
+}
+
+func (u *User) Create() {
+	//TODO
+}
+func (u *User) Read() {
+	//TODO
+}
+func (u *User) Update() {
+	//TODO
+}
+func (u *User) Delete() {
+	//TODO
+}
